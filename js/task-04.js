b407500011ae9303f6368e8674eb73cdb35d984e
Iryna Husieva <02guseva02@gmail.com>
2023-05-05 12:10:25 +0200

    5tasks

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,13 @@
-const counterValue = 0;
+let counterValue = 0;
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const counterSpan = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', () => {
+    counterValue = counterValue - 1;
+    counterSpan.textContent = counterValue; 
+});
+incrementBtn.addEventListener('click', () => {
+    counterValue = counterValue + 1;
+    counterSpan.textContent = counterValue;
+});
