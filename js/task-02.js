cc6b7c2e253adb1b8627b66957709cfa622423c9
Iryna Husieva <02guseva02@gmail.com>
2023-05-06 19:39:05 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,26 @@ const ingredients = [
   'Condiments',
 ];
 
-const ref = document.querySelector('#ingredients');
+// const ref = document.querySelector('#ingredients');
 
-ingredients.forEach(ingredient => {
-  const createList = document.createElement('li');
-  createList.classList.add('item');
-  createList.textContent = ingredient;
-  ref.appendChild(createList);
-});
+// ingredients.forEach(ingredient => {
+//   const createList = document.createElement('li');
+//   createList.classList.add('item');
+//   createList.textContent = ingredient;
+//   ref.appendChild(createList);
+// });
+
+// const createList = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`).join('');
+// ref.innerHTML = createList;
+
+const ulRef = document.querySelector('#ingredients'); 
+const liElements = [];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const createLi = document.createElement('li'); 
+  createLi.textContent = ingredients[i];
+  createLi.classList.add('item');
+  liElements.push(createLi); 
+}
+
+ulRef.append(...liElements);
